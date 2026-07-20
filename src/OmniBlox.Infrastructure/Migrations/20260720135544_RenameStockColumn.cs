979fc88d2ac03a7b8e8f8fac3209f657a82914a4
Dev-Yousaf <muhammadyousuf575@gmail.com>
2026-07-20 18:56:34 +0500

    fix: add migration to rename StockQuantity column to Stock

---
+++ src/OmniBlox.Infrastructure/Migrations/20260720135544_RenameStockColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmniBlox.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameStockColumn : Migration
+    {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.RenameColumn(
+            name: "StockQuantity",
+            table: "Products",
+            newName: "Stock");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.RenameColumn(
+            name: "Stock",
+            table: "Products",
+            newName: "StockQuantity");
+    }
+    }
+}
